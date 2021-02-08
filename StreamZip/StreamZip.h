5c872bfb439d8fa821dda4aafbfb2e91fac625af
DJ.HAN <djhan@kbdmania.net>
2021-02-08 11:03:41 +0900

    Initial Commit

---
+++ StreamZip/StreamZip.h
@@ -0,0 +1,18 @@
+//
+//  StreamZip.h
+//  StreamZip
+//
+//  Created by DJ.HAN on 2021/02/08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StreamZip.
+FOUNDATION_EXPORT double StreamZipVersionNumber;
+
+//! Project version string for StreamZip.
+FOUNDATION_EXPORT const unsigned char StreamZipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamZip/PublicHeader.h>
+
+
